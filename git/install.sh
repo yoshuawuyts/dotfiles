d2b362b7496f8cd19cffa4fae5c080b0425dadbe
Yosh <github@yosh.is>
2025-08-12 02:35:23 +0200

    add layout files to gitignore

---
+++ git/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dirname="$(dirname "$(readlink -f "$0")")"
+source "$dirname/../scripts/_shared.sh"
+
+mkdir -p "$HOME/.config/git/"
+
+src="$dirname/gitignore"
+dst="$HOME/.config/git/gitignore"
+_link "$src" "$dst"
