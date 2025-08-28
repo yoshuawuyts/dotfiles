676198742dd51639bc1593048c413dea764ddcd1
Yosh <github@yosh.is>
2025-08-29 01:57:34 +0200

    reorg snacks.nvim

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,13 +1,4 @@
-return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = {
-        preset = {
-          pick = function(cmd, opts)
-            return LazyVim.pick(cmd, opts)()
-          end,
-          header = [[
+local logo = [[
         _..---...,""-._     ,/}/)
      .''        ,      ``..'(/-<
     /   _      {      )         \
@@ -15,8 +6,34 @@ return {
  ,'   ( \  )      `.  \ __.._ .: y
 (  <\_-) )'-.____...\  `._   //-'
  `. `-' /-._)))      `-._)))
-   `...'         
-]],
+   `...'
+]]
+
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker.files()" },
+            -- { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.picker.recent()" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.picker.files({cwd = vim.fn.stdpath('config')})",
+            },
+            -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+          header = logo,
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
         },
       },
     },
