cdf3f415589386ff218edff358f5d910cacaa986
Yosh <github@yosh.is>
2025-10-08 16:22:36 +0200

    update nvim

--- nvim/lua/plugins/mini-map.lua
+++ nvim/lua/plugins/mini-map.lua
@@ -1,75 +1,75 @@
---- Based on
---- https://github.com/qiine/qiine-nvim-conf/blob/17a429ff49ab04447af623d532c0311bffa9a228/lua/plugins/ui/editor/mini-map.lua#L4
-
-return {
-  "nvim-mini/mini.map",
-  enabled = true,
-
-  config = function()
-    local map = require("mini.map")
-    require("mini.map").setup({
-      integration = nil,
-
-      -- Symbols used to display data
-      symbols = {
-        -- Encode symbols. See `:h MiniMap.config` for specification and
-        -- `:h MiniMap.gen_encode_symbols` for pre-built ones.
-        -- Default: solid blocks with 3x2 resolution.
-        -- stylua: ignore
-                encode = {
-        '⠀', '⠁', '⠈', '⠉', '⠂', '⠃', '⠊', '⠋', '⠐', '⠑', '⠘', '⠙', '⠒', '⠓', '⠚', '⠛',
-        '⠄', '⠅', '⠌', '⠍', '⠆', '⠇', '⠎', '⠏', '⠔', '⠕', '⠜', '⠝', '⠖', '⠗', '⠞', '⠟',
-        '⠠', '⠡', '⠨', '⠩', '⠢', '⠣', '⠪', '⠫', '⠰', '⠱', '⠸', '⠹', '⠲', '⠳', '⠺', '⠻',
-        '⠤', '⠥', '⠬', '⠭', '⠦', '⠧', '⠮', '⠯', '⠴', '⠵', '⠼', '⠽', '⠶', '⠷', '⠾', '⠿',
-        '⡀', '⡁', '⡈', '⡉', '⡂', '⡃', '⡊', '⡋', '⡐', '⡑', '⡘', '⡙', '⡒', '⡓', '⡚', '⡛',
-        '⡄', '⡅', '⡌', '⡍', '⡆', '⡇', '⡎', '⡏', '⡔', '⡕', '⡜', '⡝', '⡖', '⡗', '⡞', '⡟',
-        '⡠', '⡡', '⡨', '⡩', '⡢', '⡣', '⡪', '⡫', '⡰', '⡱', '⡸', '⡹', '⡲', '⡳', '⡺', '⡻',
-        '⡤', '⡥', '⡬', '⡭', '⡦', '⡧', '⡮', '⡯', '⡴', '⡵', '⡼', '⡽', '⡶', '⡷', '⡾', '⡿',
-        '⢀', '⢁', '⢈', '⢉', '⢂', '⢃', '⢊', '⢋', '⢐', '⢑', '⢘', '⢙', '⢒', '⢓', '⢚', '⢛',
-        '⢄', '⢅', '⢌', '⢍', '⢆', '⢇', '⢎', '⢏', '⢔', '⢕', '⢜', '⢝', '⢖', '⢗', '⢞', '⢟',
-        '⢠', '⢡', '⢨', '⢩', '⢢', '⢣', '⢪', '⢫', '⢰', '⢱', '⢸', '⢹', '⢲', '⢳', '⢺', '⢻',
-        '⢤', '⢥', '⢬', '⢭', '⢦', '⢧', '⢮', '⢯', '⢴', '⢵', '⢼', '⢽', '⢶', '⢷', '⢾', '⢿',
-        '⣀', '⣁', '⣈', '⣉', '⣂', '⣃', '⣊', '⣋', '⣐', '⣑', '⣘', '⣙', '⣒', '⣓', '⣚', '⣛',
-        '⣄', '⣅', '⣌', '⣍', '⣆', '⣇', '⣎', '⣏', '⣔', '⣕', '⣜', '⣝', '⣖', '⣗', '⣞', '⣟',
-        '⣠', '⣡', '⣨', '⣩', '⣢', '⣣', '⣪', '⣫', '⣰', '⣱', '⣸', '⣹', '⣲', '⣳', '⣺', '⣻',
-        '⣤', '⣥', '⣬', '⣭', '⣦', '⣧', '⣮', '⣯', '⣴', '⣵', '⣼', '⣽', '⣶', '⣷', '⣾', '⣿',
-        resolution = { row = 4, col = 2 } },
-        --aaaaaa  a      --█  ╏a
-        -- Scrollbar parts for view and line. Use empty string to disable any.
-        scroll_line = "|",
-        scroll_view = "",
-      },
-      window = {
-        -- Whether window is focusable in normal way (with `wincmd` or mouse)
-        focusable = false,
-
-        -- Side to stick ('left' or 'right')
-        side = "right",
-
-        -- Whether to show count of multiple integration highlights
-        show_integration_count = true,
-
-        -- Total width
-        width = 11,
-
-        -- Value of 'winblend' option
-        winblend = 75,
-
-        -- Z-index
-        zindex = 10,
-      },
-    })
-
-    vim.api.nvim_create_autocmd("BufEnter", {
-      group = "UserAutoCmds",
-      pattern = "*",
-      callback = function()
-        require("mini.map").open()
-      end,
-    })
-
-    vim.api.nvim_create_user_command("ToggleMinimap", function()
-      require("mini.map").toggle()
-    end, {})
-  end,
-}
+--- --- Based on
+--- --- https://github.com/qiine/qiine-nvim-conf/blob/17a429ff49ab04447af623d532c0311bffa9a228/lua/plugins/ui/editor/mini-map.lua#L4
+---
+--- return {
+---   "nvim-mini/mini.map",
+---   enabled = true,
+---
+---   config = function()
+---     local map = require("mini.map")
+---     require("mini.map").setup({
+---       integration = nil,
+---
+---       -- Symbols used to display data
+---       symbols = {
+---         -- Encode symbols. See `:h MiniMap.config` for specification and
+---         -- `:h MiniMap.gen_encode_symbols` for pre-built ones.
+---         -- Default: solid blocks with 3x2 resolution.
+---         -- stylua: ignore
+---                 encode = {
+---         '⠀', '⠁', '⠈', '⠉', '⠂', '⠃', '⠊', '⠋', '⠐', '⠑', '⠘', '⠙', '⠒', '⠓', '⠚', '⠛',
+---         '⠄', '⠅', '⠌', '⠍', '⠆', '⠇', '⠎', '⠏', '⠔', '⠕', '⠜', '⠝', '⠖', '⠗', '⠞', '⠟',
+---         '⠠', '⠡', '⠨', '⠩', '⠢', '⠣', '⠪', '⠫', '⠰', '⠱', '⠸', '⠹', '⠲', '⠳', '⠺', '⠻',
+---         '⠤', '⠥', '⠬', '⠭', '⠦', '⠧', '⠮', '⠯', '⠴', '⠵', '⠼', '⠽', '⠶', '⠷', '⠾', '⠿',
+---         '⡀', '⡁', '⡈', '⡉', '⡂', '⡃', '⡊', '⡋', '⡐', '⡑', '⡘', '⡙', '⡒', '⡓', '⡚', '⡛',
+---         '⡄', '⡅', '⡌', '⡍', '⡆', '⡇', '⡎', '⡏', '⡔', '⡕', '⡜', '⡝', '⡖', '⡗', '⡞', '⡟',
+---         '⡠', '⡡', '⡨', '⡩', '⡢', '⡣', '⡪', '⡫', '⡰', '⡱', '⡸', '⡹', '⡲', '⡳', '⡺', '⡻',
+---         '⡤', '⡥', '⡬', '⡭', '⡦', '⡧', '⡮', '⡯', '⡴', '⡵', '⡼', '⡽', '⡶', '⡷', '⡾', '⡿',
+---         '⢀', '⢁', '⢈', '⢉', '⢂', '⢃', '⢊', '⢋', '⢐', '⢑', '⢘', '⢙', '⢒', '⢓', '⢚', '⢛',
+---         '⢄', '⢅', '⢌', '⢍', '⢆', '⢇', '⢎', '⢏', '⢔', '⢕', '⢜', '⢝', '⢖', '⢗', '⢞', '⢟',
+---         '⢠', '⢡', '⢨', '⢩', '⢢', '⢣', '⢪', '⢫', '⢰', '⢱', '⢸', '⢹', '⢲', '⢳', '⢺', '⢻',
+---         '⢤', '⢥', '⢬', '⢭', '⢦', '⢧', '⢮', '⢯', '⢴', '⢵', '⢼', '⢽', '⢶', '⢷', '⢾', '⢿',
+---         '⣀', '⣁', '⣈', '⣉', '⣂', '⣃', '⣊', '⣋', '⣐', '⣑', '⣘', '⣙', '⣒', '⣓', '⣚', '⣛',
+---         '⣄', '⣅', '⣌', '⣍', '⣆', '⣇', '⣎', '⣏', '⣔', '⣕', '⣜', '⣝', '⣖', '⣗', '⣞', '⣟',
+---         '⣠', '⣡', '⣨', '⣩', '⣢', '⣣', '⣪', '⣫', '⣰', '⣱', '⣸', '⣹', '⣲', '⣳', '⣺', '⣻',
+---         '⣤', '⣥', '⣬', '⣭', '⣦', '⣧', '⣮', '⣯', '⣴', '⣵', '⣼', '⣽', '⣶', '⣷', '⣾', '⣿',
+---         resolution = { row = 4, col = 2 } },
+---         --aaaaaa  a      --█  ╏a
+---         -- Scrollbar parts for view and line. Use empty string to disable any.
+---         scroll_line = "|",
+---         scroll_view = "",
+---       },
+---       window = {
+---         -- Whether window is focusable in normal way (with `wincmd` or mouse)
+---         focusable = false,
+---
+---         -- Side to stick ('left' or 'right')
+---         side = "right",
+---
+---         -- Whether to show count of multiple integration highlights
+---         show_integration_count = true,
+---
+---         -- Total width
+---         width = 11,
+---
+---         -- Value of 'winblend' option
+---         winblend = 75,
+---
+---         -- Z-index
+---         zindex = 10,
+---       },
+---     })
+---
+---     vim.api.nvim_create_autocmd("BufEnter", {
+---       group = "UserAutoCmds",
+---       pattern = "*",
+---       callback = function()
+---         require("mini.map").open()
+---       end,
+---     })
+---
+---     vim.api.nvim_create_user_command("ToggleMinimap", function()
+---       require("mini.map").toggle()
+---     end, {})
+---   end,
+--- }
